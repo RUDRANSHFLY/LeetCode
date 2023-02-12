531369d7264421f79855910ee72a1966bc3f9604
TORNEDO <rokudokubaradiyabrothers@gmail.com>
2023-02-12 06:48:30 +0530

    INSERTION SORT ADDED IN C

---
+++ Sorting/Insertion_Sort/insert_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+static void printArray(int arr[],int s){
+    for(int i = 0 ; i < s ; i++){
+        printf("%d " ,arr[i]);
+    }
+    printf("\n");
+}
+
+static void insert_sort(int arr[] , int s){
+    for(int i = 0 ; i < s - 1 ; i++ ){
+        int sm = i ;
+        for(int j = i + 1 ; j < s ; j++){
+            if(arr[sm] > arr[j]){
+                sm = j ;
+            }
+        }
+        int t = arr[sm] ;
+        arr[sm] = arr[i];
+        arr[i] = t ; 
+    }
+}
+
+int main(){
+    printf("Enter the size of Array :-");
+    int size ; 
+    scanf("%d",&size);
+    int arr[size];
+    for(int i = 0 ; i < size ; i++){
+        arr[i] = scanf("%d",&i);
+    }
+    printf("THE INPUT ARRAY : - ");
+    printArray(arr,size);
+    insert_sort(arr,size);
+    printf("THE OUTPUT ARRAY :- ");
+    printArray(arr,size);
+    return 0 ;
+}
